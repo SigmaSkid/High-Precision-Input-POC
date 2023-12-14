61e5bcbac1646029922884a4b9d516ea9b5ffe06
Sigma <51486191+SigmaSkid@users.noreply.github.com>
2023-12-15 00:06:00 +0100

    Account for spray
    The previous solution was naive in the way it handled sprays, only the first fired bulled would be extremely accurate.
    
    This commit makes the spray sub-subtick accurate. Or whatever I should call it. The imaginary 'bullets' will now go where they're supposed to.

--- High Precision Input POC/main.cpp
+++ High Precision Input POC/main.cpp
@@ -6,13 +6,34 @@
 #include <algorithm>
 #include <thread> // use windows threads? cuz I have windows defines? HAHA 
 
-std::chrono::high_resolution_clock::time_point TheBeginningOfTime;
-std::chrono::high_resolution_clock::time_point LastShotTime;
+// this proof of concept assumes an automatic weapon.
 
 // adjust it for yourself, I use 20k dpi, this POC utilizes raw input!
 #define sensitivity 0.0001
-#define simulated_tickrate 16
+// low tickrate makes it easy to read debug logs in real time.
+// also it demonstrates pretty well, why this input method is based, as inputs
+// even with the tickrate of 1, will be as precise as they can be.
+#define simulated_tickrate 1
+// ak47 - 600
+#define RPM 600
 #define MeasureTimeDeltaMS(A) std::chrono::duration_cast<std::chrono::milliseconds>(A).count()
+
+
+// converting RPM to something useful.
+constexpr std::chrono::milliseconds rpmToMilliseconds(int rpm) {
+    return std::chrono::milliseconds{ static_cast<long long>((60 * 1000) / rpm) };
+}
+
+// convert simulated tickrate to time between ticks accurate to the microsecond
+constexpr long long tickToDelay(long double tickrate) {
+    return { static_cast<long long>( ( 1./tickrate ) * 1000. * 1000.) };
+}
+
+std::chrono::high_resolution_clock::time_point TheBeginningOfTime;
+std::chrono::high_resolution_clock::time_point m_flNextPrimaryAttack;
+
+constexpr auto shotDelay = rpmToMilliseconds(RPM);
+constexpr long long tickDelta = tickToDelay(simulated_tickrate);
 
 struct MouseEvent {
     LONG horizontal;
@@ -38,9 +59,13 @@ struct SimplifiedDataPacket {
 struct BasedPlayer {
     double yaw = 0.;
     double pitch = 0.;
-    BOOL leftMouseDown = false;
+
     // could also be done for scoping to get the accurate accuracy penalty
     // but i'm too lazy to add that in this POC
+    BOOL leftMouseDown = false;
+
+    // when was the player last locally simulated?
+    std::chrono::high_resolution_clock::time_point timestamp;
 
     void normalizeViewAngles()
     {
@@ -67,8 +92,13 @@ HWND hwnd;
 int main()
 {
     TheBeginningOfTime = std::chrono::high_resolution_clock::now();
-    LastShotTime = TheBeginningOfTime;
+    m_flNextPrimaryAttack = TheBeginningOfTime;
 
+    std::cout << "RPM:   " << RPM << std::endl
+              << "Delay between shots: " << shotDelay << std::endl // chrono puts 'ms' for us <3
+              << "Tickrate:   " << simulated_tickrate << " hz" << std::endl
+              << "Delay between tick: " << tickDelta << " microseconds" << std::endl << std::endl;
+    
     std::thread a1(TickHandler); 
     std::thread a2(DataCollector);
     std::thread a3(ServerWannabe);
@@ -83,62 +113,155 @@ int main()
     return -1;
 }
 
+bool canShoot(const BasedPlayer& jeff69)
+{
+    auto time_delta = MeasureTimeDeltaMS(jeff69.timestamp - m_flNextPrimaryAttack);
+    
+    // not enough time passed since last shot
+    if (time_delta < 0)
+        return false;
+
+    return true;
+}
+
+ImportantEvent shoot(BasedPlayer& jeff69)
+{
+    jeff69.normalizeViewAngles();
+
+    ImportantEvent event_;
+    event_.pitch = jeff69.pitch;
+    event_.yaw = jeff69.yaw;
+    event_.timestamp = jeff69.timestamp;
+
+    return event_;
+}
+
+// in the case of BEAM weapons, (they don't exist in counter strike, but in halo or something else maybe)
+// this would just have to check when we started and when we ended holding mouse1
 std::vector<ImportantEvent> DataMuncher(std::vector<MouseEvent>& mouseEvents)
 {
     std::vector<ImportantEvent> ImportantEvents;
+    std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
+    
+    // this is really just used for keeping track of yaw & pitch, could also be used for something like scope sensitivity tho.
+    // cuz I'm lazy, we will just copy the entire localPlayer for each event, this could be expensive in the context 
+    // of a fully featured game.. but for me it's basically free, so I don't need to apply the aforementioned optimizations.
+    std::vector<BasedPlayer> playerStateRecord;
 
+    // let's start by adding the state from the previous tick to our nice vector.
+    playerStateRecord.push_back(localPlayer);
+
+    // create a timeline of events.
+    // so we can do the shooty shoot accurately, with as real time precision as really possible.
+    // the important thing we log here is the way leftMouseDown local player variable should change over time
     for (const auto& me : mouseEvents)
     {
+        // look at the comments above, we really only care about some stuff from this struct here.
+        BasedPlayer simulatedPlayer = playerStateRecord.back();
+
+        // if mouse state = didn't change (0) then don't bother doing anything.
+        // if mouse state = mouse key went down
         if (me.leftMouseState == 1)
         {
-            localPlayer.leftMouseDown = TRUE;
+            simulatedPlayer.leftMouseDown = TRUE;
         }
-        if (me.leftMouseState == 2)
+
+        // if mouse state = mouse key went up
+        else if (me.leftMouseState == 2)
         {
-            localPlayer.leftMouseDown = FALSE;
+            simulatedPlayer.leftMouseDown = FALSE;
         }
 
-        auto delta = MeasureTimeDeltaMS(me.timestamp - LastShotTime);
+        // this math could be skipped, by just saving the mouse state and timestamp
+        // and then doing this later on, only for the shots, but it doesn't really matter
+        // plus this is easier for me to do in a POC
+        simulatedPlayer.yaw += me.horizontal * sensitivity;
+        simulatedPlayer.pitch += me.vertical * sensitivity;
+        simulatedPlayer.timestamp = me.timestamp;
 
-        if (localPlayer.leftMouseDown &&
-            // shot every 100ms, cuz ak47 is 600rpm
-            delta >= 100)
-        {
-            localPlayer.normalizeViewAngles();
-
-            // we shoot at this timestamp, (that's between frames), and start rendering our shot on this frame already
-            ImportantEvent event_;
-            event_.pitch = localPlayer.pitch;
-            event_.yaw = localPlayer.yaw;
-            event_.timestamp = me.timestamp;
-
-            ImportantEvents.push_back(event_);
-
-            LastShotTime = me.timestamp;
-        }
-
-        localPlayer.yaw += me.horizontal * sensitivity;
-        localPlayer.pitch += me.vertical * sensitivity;
+        // store our bad boi sub-subtick player for future sub-subticks reference
+        playerStateRecord.push_back(simulatedPlayer);
     }
 
+    // this is to account for us receiving 0 mouse inputs 
+    // which can happen if you're holding mouse1 while having your mouse in air, or if you're not overdosing caffeine....
+    BasedPlayer eternalFool = playerStateRecord.back();
+    eternalFool.timestamp = now;
+    playerStateRecord.push_back(eternalFool);
+
+    // now let's delete the player state from the last frame.
+    // (we needed this fool, in the last loop)
+    playerStateRecord.erase(playerStateRecord.begin());
+
+    // used in the loop for checking if we shot between events
+    auto& lastPlayerState = localPlayer;
+
+    // now let's actually check when the shots happened. shall we?
+    for (auto& playerState : playerStateRecord)
+    {
+        // we're trying to shoot, nice.
+        if (playerState.leftMouseDown)
+        
+        // we can shoot, nice.
+        if (canShoot(playerState))
+        {
+            // did we not try to shoot on the last update?
+            if (!lastPlayerState.leftMouseDown)
+            {
+                // if we didn't then just shoot now.
+                // we already checked that we can.
+                auto shot = shoot(playerState);
+                ImportantEvents.push_back(shot);
+
+                // we just started shooting so adjust next shot delay accordingly.
+                m_flNextPrimaryAttack = shot.timestamp + shotDelay;
+            }
+            // okay, we tried to shoot already,
+            // now we're just holding mouse1. 
+            // let's see, if the next shot should have already happened.
+            else
+            {
+                auto time_delta = MeasureTimeDeltaMS(playerState.timestamp - m_flNextPrimaryAttack);
+
+                // our shot should actually happen on this exact player state, down to the milisecond?
+                // good enough for me.
+                if (time_delta == 0)
+                {
+                    auto shot = shoot(playerState);
+                    ImportantEvents.push_back(shot);
+
+                    m_flNextPrimaryAttack += shotDelay;
+                }
+                // a shot should have happened between this update and the previous one
+                else if (time_delta > 0)
+                {
+                    // so.. let's check when it happened, with the precision down to the milisecond
+                    auto PreciseShotTiming = playerState.timestamp - std::chrono::milliseconds(time_delta);
+
+                    // and just assume the previous mouse position is where we shot,
+                    // don't interpolate this, because if the mouse moved, then was pressed, then moved
+                    // we don't care about the state from after it was pressed.
+                    BasedPlayer hypotheticalPlayer = lastPlayerState;
+                    hypotheticalPlayer.timestamp = PreciseShotTiming;
+                    auto shot = shoot(hypotheticalPlayer);
+                    ImportantEvents.push_back(shot);
+
+                    m_flNextPrimaryAttack += shotDelay;
+                }
+                // else if (time delta < 0)
+                // will never happen because of the canShoot check,
+                // in the present, we don't care about us being able to shoot in the future
+            }
+        }
+
+        lastPlayerState = playerState;
+    }
+
+    // now give the results of our hard work to the local player.
+    localPlayer = lastPlayerState;
+    
     // # anti untrusted
     localPlayer.normalizeViewAngles();
-
-    // in case we got 0 mouse events, still do the tick/frame based update, so gun fires and stuff
-    // this can happen if you're holding mouse1 while having your mouse in air, or if you're not overdosing caffeine....
-    std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
-    auto time_delta = MeasureTimeDeltaMS(now - LastShotTime);
-
-    if (localPlayer.leftMouseDown && time_delta >= 100)
-    {
-        ImportantEvent event_;
-        event_.pitch = localPlayer.pitch;
-        event_.yaw = localPlayer.yaw;
-        event_.timestamp = now;
-
-        ImportantEvents.push_back(event_);
-        LastShotTime = now;
-    }
 
     return ImportantEvents;
 }
@@ -160,7 +283,7 @@ DWORD WINAPI TickHandler()
         auto ms = std::chrono::duration_cast<std::chrono::microseconds>(delta).count();
 
         // wait for next tick/frame (which isn't real in this POC)
-        if (ms < (1./simulated_tickrate) * 1000 * 1000)
+        if (ms < tickDelta)
             continue;
 
         last = now;
@@ -183,27 +306,51 @@ DWORD WINAPI TickHandler()
         // output << "Just a reminder that this proof of concept creates a transparent fullscreen window, use alt+f4 if you need to exit" << std::endl;
         // who needs these disclaimers being printed anyway, am i right?
 
+        // don't report our inputs if we didn't do anything.
+
+        output << std::endl << "----TICK START----" << std::endl;
+
+        if (MouseEventsCopy.size() > 0)
         output << "Received " << MouseEventsCopy.size() << " mouse events since last tick." << std::endl;
 
-        // also updates yaw and pitch, so print these first
         SimplifiedDataPacket HilariousPacket;
 
+        // also updates local player.
         HilariousPacket.shots = DataMuncher(MouseEventsCopy);
 
-        output << "Current YAW: " << localPlayer.yaw << "   PITCH: " << localPlayer.pitch << std::endl << std::endl << std::endl;
+        auto timeToNextShot = MeasureTimeDeltaMS(m_flNextPrimaryAttack - now);
+        auto WeCanShoot = canShoot(localPlayer);
 
-
-        // imagine you're playing the game, while seeing numbers that represent it.
-        // imagine that my proof of concept project looks more photorealistic than any game you have ever seen.
-        std::cout << output.str();
+        output << "Current YAW: " << localPlayer.yaw << "   PITCH: " << localPlayer.pitch
+            << "  CURTIME: " << MeasureTimeDeltaMS(now - TheBeginningOfTime) << std::endl;
         
+        if (WeCanShoot)
+            output << " CAN SHOOT" << std::endl;
+        else
+            output << " Next shot in " << timeToNextShot << " ms" << std::endl;
+
+        for (auto& shot : HilariousPacket.shots)
+        {
+            output << "logged shot: Y: " << shot.yaw << " P: " << shot.pitch << " T: " << MeasureTimeDeltaMS(shot.timestamp - TheBeginningOfTime) << std::endl;
+        }
+        output << "----TICK END----" << std::endl << std::endl;
+
+
+
         // *send* the data over our imaginary network *hotline*
         {
             std::lock_guard<std::mutex> guard(realisticNetworkMutex);
             OurImaginaryBasedNetworkingStack.push_back(HilariousPacket);
         }
 
+        // imagine you're playing the game, while seeing numbers that represent it.
+        // imagine that my proof of concept project looks more photorealistic than any game you have ever seen.
+        std::cout << output.str();
+
+
         // now do the other shit, like render the game poorly etc.
+        // if we started shooting between frames/ticks you can interpolate the shooting animation
+        // (tip: we probably, most likely, I'm 100% sure, that we did.)
     }
     return 0;
 }
@@ -320,7 +467,7 @@ DWORD WINAPI ServerWannabe() {
         auto ms = std::chrono::duration_cast<std::chrono::microseconds>(delta).count();
 
         // wait for next tick/frame (which isn't real in this POC)
-        if (ms < (1. / simulated_tickrate) * 1000 * 1000)
+        if (ms < tickDelta)
             continue;
 
         last = now;
@@ -351,9 +498,21 @@ DWORD WINAPI ServerWannabe() {
                 // do the shooty shoot and interpolate the players using the shots timestamp,
                 // in the case of a real server using a chrono timestamp is dumb af, so just do a double with the
                 // amount of interpolation needed between player positions in tick A and tick B
+                // (THIS is tick B, tick A is the previous tick)
                 // something along the lines of InterpolatePos(PosA, PosB, Fraction)
-                std::cout << "Server registered the shot!" << std::endl;
+                // obviously remember to also interpolate the shooting position, even tho, it probably really doesn't matter
+                // cuz if you're moving you're prolly gonna miss anyway in the case of cs2.
+                std::cout << "Server registered the shot!" << std::endl
+                          << "  Yaw: "    << shot.yaw 
+                          << "  Pitch: "  << shot.pitch 
+                          << "  Time: "   << MeasureTimeDeltaMS(shot.timestamp - TheBeginningOfTime) 
+                          << std::endl << std::endl;
 
+                // if you're paranoid and want to feed your AI anticheat more data,
+                // you could just send all of the mouse inputs, but imho it's a waste of cpu time, storage, bandwidth etc.
+                // so we're just sending the important ones, the server should be able to use the data we gave it,
+                // to check whether we could actually have shot during these timestamps, to prevent abuse of this system.
+                // I'm sure the AI would love all of that data tho...
             }
         }
         if (UserForceQuitCollector)
